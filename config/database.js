747f70e7012cc70b61bef4a9c2393d33ac375eac
shivamdesale394 <shivamdesale1234@gmail.com>
2024-11-23 15:03:25 +0530

    Update database.js

--- config/database.js
+++ config/database.js
@@ -1,25 +1,11 @@
-import mongoose from 'mongoose';
-import dotenv from 'dotenv';
+import mongoose from "mongoose";
 
-dotenv.config(); // This loads variables from .env into process.env
-
-const dbConnect = async () => {
-  try {
-    const uri = process.env.MONGODB_URL;
-    if (!uri) {
-      throw new Error('MONGODB_URL is undefined');
+const dbConnect=async()=>{
+    try {
+       await mongoose.connect(process.env.MONGODB_URL)
+       console.log("MongoDB connected successfully")
+    } catch (error) {
+        console.log("Connection failed",error)
     }
-
-    await mongoose.connect(uri, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
-
-    console.log('MongoDB connected successfully!');
-  } catch (error) {
-    console.error(`Connection Failed: ${error.message}`);
-    process.exit(1);
-  }
-};
-
+}
 export default dbConnect;
